57b16ba8a07178b7582403dcdaaff19d3b3303dc
yaswanth <29805674+yaswanth97531@users.noreply.github.com>
2019-05-14 12:59:17 -0500

    Update DataTable.jsx

--- src/Containers/DataTable/DataTable.jsx
+++ src/Containers/DataTable/DataTable.jsx
@@ -1,1 +1,24 @@
-
+import React from 'react';
+const DataTable = props => {
+   return (
+      <table>
+         <thead>
+            <tr>
+               {props.cols.map(col =>
+                  <th key={col.name}>{col.header}</th>
+               )}
+            </tr>
+         </thead>
+         <tbody>
+            {props.rows.map(row =>
+               <tr key={row.id}>
+                   {props.cols.map(col =>
+                       <td key={col.name}>{row[col.name]}</td>
+                   )}
+               </tr>
+            )}
+         </tbody>
+      </table>
+  );
+}
+export default DataTable;
